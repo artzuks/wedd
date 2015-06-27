c494ccae3180471867b6b45b92f33f2d7782cef0
Art Zuks <zuks.art@gmail.com>
2015-06-27 13:53:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ app.get('/',function(req,res){
 
 });
 
-var server = app.listen(443,function(){
+app.set('port', 80);
+
+var server = app.listen(80),function(){
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('Example app listening on port 443');
+	console.log('Example app listening on port 80');
 });
