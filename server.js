665ccc83e2f1d365f3867ce2001bd2d8a0156aff
Art Zuks <zuks.art@gmail.com>
2015-08-03 19:55:43 -0400

    commiting files

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/',function(req,res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile('index.html');
 
 });
 
-var server = app.listen(443,function(){
+var server = app.listen(80,function(){
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('Example app listening on port 443');
+	console.log('Example app listening on port 80');
 });
